5c7178914413de51446bc673e6b876d9ac307be6
Tim Strackbein <tim.strackbein@gmail.com>
2020-07-17 12:06:08 -0400

    added -i modifier to sed command

--- koan.sh
+++ koan.sh
@@ -2,7 +2,7 @@
 hassioStatus=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8123)
 if [ $hassioStatus -ne 200 ]; then
     {
-        sed 's#{{ secrets.AMCRESTPASSWORD }}#password#g' homeassistant/config/secrets.yaml
+        sed -i 's#{{ secrets.AMCRESTPASSWORD }}#password #g' homeassistant/config/secrets.yaml
         docker-compose up -d
     }
 else
