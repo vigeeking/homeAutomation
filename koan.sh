0d6b9c966186375373d4eec965a8b06d0d116c16
Tim Strackbein <tim.strackbein@gmail.com>
2020-07-17 11:56:51 -0400

    cleanup

--- koan.sh
+++ koan.sh
@@ -2,7 +2,7 @@
 hassioStatus=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8123)
 if [ $hassioStatus -ne 200 ]; then
     {
-        sed 's#{{ secrets.AMCRESTPASSWORD }}#password#g' homeassistant/secrets.yaml
+        sed 's#{{ secrets.AMCRESTPASSWORD }}#password#g' homeassistant/config/secrets.yaml
         docker-compose up -d
     }
 else
