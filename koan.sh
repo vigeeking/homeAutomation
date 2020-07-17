07281c1c488806214b25cd2d9abed93d80ba66ff
Tim Strackbein <tim.strackbein@gmail.com>
2020-07-17 10:03:36 -0400

    modified koan.sh

--- koan.sh
+++ koan.sh
@@ -1,4 +1,8 @@
 #Check to see if home assistant is running
 echo -n "what is the ip address of Home Assistant?" 
 read hassio
-curl "$hassio:8123" 
+curl "$hassio:8123"
+echo -n "is this being run locally?"
+read local
+if [ "$local"   
+sed 's#{{ secrets.AMCRESTPASSWORD }}#password#g' homeassistant/secrets.yaml
